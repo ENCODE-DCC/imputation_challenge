f4b96840ab91f686e0531314a078765ddf12b468
Jin Lee <leepc12@gmail.com>
2019-05-13 19:34:03 -0700

    strip ' from filename

--- validate.py
+++ validate.py
@@ -116,7 +116,7 @@ def main():
     args = parse_arguments()
 
     log.info('Opening bigwig file...')
-    bw = pyBigWig.open(args.bw)
+    bw = pyBigWig.open(args.bw.strip("'"))
 
     return validate(bw)
 
