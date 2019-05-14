437712d9db863c736f2819c0be158280b075a05d
Jin Lee <leepc12@gmail.com>
2019-05-14 12:43:15 -0700

    tested rank.py with avocado chr21 only

--- rank.py
+++ rank.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Imputation challenge rankimg script
+"""Imputation challenge rankigg script
 
 Author:
-    Jin Lee (leepc12@gmail.com)
+    Jin Lee (leepc12@gmail.com)    
 """
 
 import sys
@@ -21,19 +21,118 @@ log = logging.getLogger(__name__)
 
 
 DB_QUERY_GET = 'SELECT * FROM {table} WHERE chroms="{chroms}" ORDER BY bootstrap_id, submission_id;'
-
-ATTRS_TO_RANK = ('mse', 'mse1obs', 'mse1imp', 'gwcorr', 'match1',
-                 'catch1obs', 'catch1imp', 'aucobs1', 'aucimp1',
-                 'mseprom', 'msegene', 'mseenh')
+CELL_NAME = {
+    "C02": "adrenal_gland",
+    "C20": "heart_left_ventricle",
+    "C35": "omental_fat_pad",
+    "C45": "testis",
+    "C05": "BE2C",
+    "C06": "brain_microvascular_endothelial_cell",
+    "C07": "Caco-2",
+    "C08": "cardiac_fibroblast",
+    "C11": "dermis_microvascular_lymphatic_vessel_endothelial_cell",
+    "C15": "G401",
+    "C16": "GM06990",
+    "C17": "H1-hESC",
+    "C18": "H9",
+    "C19": "HAP-1",
+    "C21": "hematopoietic_multipotent_progenitor_cell",
+    "C22": "HL-60",
+    "C23": "IMR-90",
+    "C24": "K562",
+    "C27": "mesenchymal_stem_cell",
+    "C28": "MG63",
+    "C30": "NCI-H460",
+    "C32": "neural_stem_progenitor_cell",
+    "C33": "occipital_lobe",
+    "C39": "SJCRH30",
+    "C40": "SJSA1",
+    "C41": "SK-MEL-5",
+    "C42": "skin_fibroblast",
+    "C43": "skin_of_body",
+    "C44": "T47D",
+    "C46": "trophoblast_cell",
+    "C47": "upper_lobe_of_left_lung",
+    "C48": "urinary_bladder",
+    "C49": "uterus",
+    "C51": "WERI-Rb-1",
+    "C12": "DND-41",
+    "C25": "KMS-11",
+    "C31": "NCI-H929",
+    "C34": "OCI-LY7",
+    "C01": "adipose_tissue",
+    "C03": "adrenal_gland_embryonic",
+    "C09": "CD4-positive_alpha-beta_memory_T_cell",
+    "C10": "chorion",
+    "C13": "endocrine_pancreas",
+    "C36": "peripheral_blood_mononuclear_cell",
+    "C37": "prostate",
+    "C38": "RWPE2",
+    "C50": "vagina",
+    "C04": "amnion",
+    "C29": "myoepithelial_cell_of_mammary_gland",
+    "C14": "ES-I3",
+    "C26": "lower_leg_skin"
+}
+ASSAY_NAME = {
+    "M01": "ATAC-seq",
+    "M02": "DNase-seq",
+    "M03": "H2AFZ",
+    "M04": "H2AK5ac",
+    "M05": "H2AK9ac",
+    "M06": "H2BK120ac",
+    "M07": "H2BK12ac",
+    "M08": "H2BK15ac",
+    "M09": "H2BK20ac",
+    "M10": "H2BK5ac",
+    "M11": "H3F3A",
+    "M12": "H3K14ac",
+    "M13": "H3K18ac",
+    "M14": "H3K23ac",
+    "M15": "H3K23me2",
+    "M16": "H3K27ac",
+    "M17": "H3K27me3",
+    "M18": "H3K36me3",
+    "M19": "H3K4ac",
+    "M20": "H3K4me1",
+    "M21": "H3K4me2",
+    "M22": "H3K4me3",
+    "M23": "H3K56ac",
+    "M24": "H3K79me1",
+    "M25": "H3K79me2",
+    "M26": "H3K9ac",
+    "M27": "H3K9me1",
+    "M28": "H3K9me2",
+    "M29": "H3K9me3",
+    "M30": "H3T11ph",
+    "M31": "H4K12ac",
+    "M32": "H4K20me1",
+    "M33": "H4K5ac",
+    "M34": "H4K8ac",
+    "M35": "H4K91ac"
+}
 
 GlobalScore = namedtuple(
     'GlobalScore',
     ['team_id', 'name', 'score_lb', 'score_mean', 'score_ub', 'rank'])
 
 
-
 def get_team_name(team_id):
     return str(team_id)
+
+
+def get_cell_name(cell_id):
+    if cell_id in CELL_NAME:
+        return CELL_NAME[cell_id].capitalize().replace('_', ' ')
+    else:
+        return str(cell_id)
+
+
+def get_assay_name(assay_id):
+    if assay_id in ASSAY_NAME:
+        return ASSAY_NAME[assay_id].capitalize().replace('_', ' ')
+    else:
+        return str(assay_id)
 
 
 def score_record_factory(cursor, row):
@@ -72,17 +171,24 @@ def read_scores_from_db(db_file, chroms):
             continue
         else:
             break
+    
+    if len(result) == 0:
+        print('No records found. '
+            'Did you forget to specify "--chrom"?')
+
     return result
 
 
-def calc_combined_ranks(rows):
+def calc_combined_ranks(rows, measures_to_use):
+    """Calculate ranks for combined measures
+    """
     # make sure that the team_id is unique
     team_ids = [x.team_id for x in rows]
     submission_ids = [x.submission_id for x in rows]
     # submission_ids = [0 for x in rows]
 
     scores = numpy.zeros(len(team_ids), dtype=float)
-    for user_i, attr in enumerate(ATTRS_TO_RANK):
+    for user_i, attr in enumerate(measures_to_use):
         attr_scores = numpy.array([getattr(x, attr) for x in rows])
         ranks = rankdata(-attr_scores, "average")
         pval_scores = numpy.log(ranks/float(len(ranks) + 1))
@@ -92,7 +198,13 @@ def calc_combined_ranks(rows):
     return dict(zip(zip(team_ids, submission_ids), ranks))
 
 
-def calc_global_ranks(rows):
+def calc_global_ranks(rows, measures_to_use):
+    """Calculate global ranks
+
+    Outputs:
+        Markdown table for ranks
+    """
+
     sample_grpd_results = defaultdict(lambda: defaultdict(list))
     all_users = set()
     for x in rows:
@@ -106,7 +218,7 @@ def calc_global_ranks(rows):
     for (cell, assay), bootstrapped_submissions in sample_grpd_results.items():
         user_ranks = defaultdict(list)
         for index, submissions in bootstrapped_submissions.items():
-            ranks = calc_combined_ranks(submissions)
+            ranks = calc_combined_ranks(submissions, measures_to_use)
 
             obs_users = set(x[0] for x in ranks.keys())
             for (team_id, submission_id), rank in ranks.items():
@@ -115,10 +227,13 @@ def calc_global_ranks(rows):
                 # user_ranks[(team_id, 0)].append(rank)
                 global_scores[index][team_id].append(
                     min(0.5, rank/(len(ranks)+1))
-                )            
+                )
             for team_id in all_users - obs_users:
-                global_scores[index][team_id].append(0.5)
+                global_scores[index][team_id].append(0.5)                
 
+        print('# {} ({})'.format(get_cell_name(cell), get_assay_name(assay)))
+        print(' | '.join(('Team name', 'rank')))
+        print('|'.join(('----',)*3))
         for (team_id, submission_id), ranks in sorted(
                 user_ranks.items(), key=lambda x: sorted(x[1])[1]):
             print('%d | %s | %.2f' % (team_id, get_team_name(team_id), sorted(ranks)[1]))
@@ -144,14 +259,11 @@ def calc_global_ranks(rows):
         ]))
     global_data = sorted(global_data, key=lambda x: (x.rank, x.score_mean))
 
-    print('\t'.join(('name', 'rank', 'lb', 'mean', 'ub')))
-    for x in global_data: 
-        print('%s\t%.2f\t%.2f\t%.2f\t%.2f' % (
-              x.name, x.rank, x.score_lb, x.score_mean, x.score_ub))
+    print()
     print('# Overall Results')
-    print(' | '.join(
-         ('Team name', 'rank', 'Lower bound', 'Mean', 'Upperbound')))
-    print('|'.join(('---',)*6))
+    print(' | '.join(('Team name', 'rank', 'Lower bound',
+                      'Mean', 'Upperbound')))
+    print('|'.join(('----',)*6))
     for x in global_data: 
         print('%s | %.2f | %.2f | %.2f | %.2f' % (
             x.name, x.rank, x.score_lb, x.score_mean, x.score_ub))
@@ -167,6 +279,11 @@ def parse_arguments():
     parser.add_argument('--chrom', nargs='+',
                         default=['all'],
                         help='List of chromosomes to be used for ranking')
+    parser.add_argument('--measures-to-use', nargs='+',
+                        default=['mse', 'mse1obs', 'mse1imp', 'gwcorr', 'match1',
+                                 'catch1obs', 'catch1imp', 'aucobs1', 'aucimp1',
+                                 'mseprom', 'msegene', 'mseenh'],
+                        help='List of performance measures to be used for ranking')
     parser.add_argument('--log-level', default='INFO',
                         choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING',
                                  'CRITICAL', 'ERROR', 'CRITICAL'],
@@ -189,7 +306,7 @@ def main():
     rows = read_scores_from_db(args.db_file, args.chrom)
 
     log.info('Calculate ranks...')
-    rv, global_data = calc_global_ranks(rows)
+    rv, global_data = calc_global_ranks(rows, args.measures_to_use)
 
     log.info('All done.')
 
