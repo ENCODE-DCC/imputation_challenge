05f08ddfe545b721e10456eecf69aeb22d51dc20
Jin Lee <leepc12@gmail.com>
2019-05-20 18:55:03 -0700

    can use blacklist for build_bootstrapped_label.py

--- build_bootstrapped_label.py
+++ build_bootstrapped_label.py
@@ -10,6 +10,7 @@ import pyBigWig
 import argparse
 import logging
 from sklearn.model_selection import StratifiedKFold
+from score import read_annotation_bed
 
 
 logging.basicConfig(
@@ -19,6 +20,9 @@ log = logging.getLogger(__name__)
 
 
 def parse_arguments():
+    import os
+    py_path = os.path.dirname(os.path.realpath(__file__))
+
     parser = argparse.ArgumentParser(
         prog='ENCODE Imputation Challenge bootstrapped label generator')
     parser.add_argument('submission_template_bw',
@@ -28,6 +32,13 @@ def parse_arguments():
                              '(n_splits for StratifiedKFold)')
     parser.add_argument('--window-size', default=25,
                         help='Window size for bigwig in bp.')
+    parser.add_argument('--blacklist-file',
+                         default=os.path.join(
+                            py_path,
+                            'annot/hg38/hg38.blacklist.bed.gz'),
+                         help='Blacklist BED file. Bootstrap label will be '
+                              'generated after removing overlapping regions '
+                              'defined in this file.')
     parser.add_argument('--random-seed', default=0,
                         help='Random seed (random_state for StratifiedKFold)')
     parser.add_argument('--out-npy-prefix', default='bootstrapped_label',
@@ -41,6 +52,33 @@ def parse_arguments():
     log.setLevel(args.log_level)
     log.info(sys.argv)
     return args
+
+
+def get_blacklisted_bins(blacklist, chroms, window_size=25):
+    """
+    Returns:
+        { chrom: [] }: label that overlaps with blackstlisted region
+    """
+    # make empty sets per chr
+    bins = {}
+    for c in chroms:
+        bins[c] = set()
+
+    for line in blacklist:
+        c, start, end = line.split()
+        start_bin_id = int(start) // window_size
+        end_bin_id = int(end) // window_size + 1
+        print(c, start, end, start_bin_id, end_bin_id)
+
+        bins[c] |= set(range(start_bin_id, end_bin_id))
+
+    # convert into list and then numpy array
+    result = {}
+    for c in chroms:
+        result[c] = numpy.array(list(bins[c]), dtype=numpy.int64)
+
+    print(result)
+    return result
 
 
 def main():
@@ -57,6 +95,12 @@ def main():
     template_bw_file = args.submission_template_bw
     bw = pyBigWig.open(template_bw_file)
 
+    log.info('Reading from blacklist bed file...')
+    blacklists = read_annotation_bed(args.blacklist_file)
+
+    blacklisted_bins = get_blacklisted_bins(blacklists, bw.chroms(),
+                                            args.window_size)
+
     # result = [dict()]*args.fold  # DANGER! IT DOES NOT WORK
     result = []
     for k in range(args.fold):
@@ -66,8 +110,13 @@ def main():
         for c, chrom_len in bw.chroms().items():
             # number of bins per chromosome
             print(c)
+            blacklist = blacklisted_bins[c]
             n_bin = (chrom_len-1)//args.window_size+1
-            result[0][c] = list(range(n_bin))
+
+            org_arr = numpy.array(range(n_bin))
+            new_arr = numpy.delete(org_arr, blacklist)
+
+            result[0][c] = new_arr
 
             for k in range(args.fold):
                 print('k={}: {}'.format(k, result[k][c][0:20]))
@@ -78,11 +127,18 @@ def main():
         for c, chrom_len in bw.chroms().items():
             # number of bins per chromosome
             print(c)
+            blacklist = blacklisted_bins[c]
             n_bin = (chrom_len-1)//args.window_size+1
+
+            org_arr = numpy.array(range(n_bin))
+            new_arr = numpy.delete(org_arr, blacklist)
+
+            n_new = n_bin - len(blacklist)
+
             for bootstrap_id, (label, _) in enumerate(sfk.split(
-                                                    numpy.zeros(n_bin),
-                                                    numpy.zeros(n_bin))):
-                result[bootstrap_id][c] = label
+                                                    numpy.zeros(n_new),
+                                                    numpy.zeros(n_new))):
+                result[bootstrap_id][c] = new_arr[label]
                 # print(bootstrap_id, result[bootstrap_id][c][0:20])
 
             for k in range(args.fold):
